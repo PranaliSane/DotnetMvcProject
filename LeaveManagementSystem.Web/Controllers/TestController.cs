0f3e94d9c1fe3cf1f143df339f90c43778d61a61
PranaliSane <pranalisane@gmail.com>
2025-11-07 18:10:33 +0530

    Completed Leave Management Module

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student"
+            };
+            return View(data);
+        }
+    } 
+}
---
+++ LeaveManagementSystem.Web/Models/TestViewModel.cs
@@ -0,0 +1,7 @@
+namespace LeaveManagementSystem.Web.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
